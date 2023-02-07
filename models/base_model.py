684a4abc48ff012ce32c2b3cd7c0f67f323e7732
frame-1 <muthee031@gmail.com>
2023-02-07 18:50:24 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -40,3 +40,12 @@ class BaseModel:
         self.updated_at = datetime.now()
         fileStorage.new(self)
         fileStorage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dict = self.__dict__.copy()
+        dict['__class___'] = self.__class__.__name__
+        for k in dict:
+            if type(dict[k]) is datetime:
+                dict[k] = dict[k].isoformat()
+        return dict
