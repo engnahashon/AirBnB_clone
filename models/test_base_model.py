32722889a89afd59d9042a4825be599fa478d0f7
Mumbi1 <mutheemumbi01@gmail.com>
2023-02-09 09:22:57 -0800

    models

--- models/test_base_model.py
+++ models/test_base_model.py
@@ -4,7 +4,7 @@ my_model = __import__('base_model').BaseModel
 my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
-my_model.save(self)
+my_model.save()
 print(my_model)
 my_model_json = my_model.to_dict()
 print(my_model_json)
