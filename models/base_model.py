c41397e108634c9f613841d04748ab36ab76e364
Mumbi1 <mutheemumbi01@gmail.com>
2023-02-12 08:26:13 -0800

    Console

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class BaseModel:
     """Base model for airbnb models"""
 
     id = Column(String(60),
-                nullable=False,
+                nullable= False,
                 primary_key=True)
     created_at = Column(DATETIME,
                         nullable=False,
@@ -20,7 +19,7 @@ class BaseModel:
                         nullable=False,
                         default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args,**kwargs):
         """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
@@ -28,10 +27,10 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             for k in kwargs:
-                if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
-                elif k! = '__class__':
-                    setattr(self, k, kwargs[k])
+                if k in ['created_at','updated_at']:
+                    setattr(self,k,datetime.fromisoformat(kwargs[k]))
+                elif k!= '__class__':
+                    setattr(self,k,kwargs[k])
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -39,14 +38,14 @@ class BaseModel:
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates attribute updated_at with the current datetime"""
+        """updates the public instance attribute updated_at with the current datetime"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
         dict = self.__dict__.copy()
         dict['__class__'] = self.__class__.__name__
         dict["created_at"] = self.created_at.isoformat()
